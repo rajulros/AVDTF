3c5aa6414b19ff836a6d27a015116c5313b4aff4
VishnuNagaPraveen <vishnunagapraveen.deepthimahanthi@lumen.com>
2024-10-14 17:15:09 +0530

    test

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "AVDDevOps"
     storage_account_name = "avddevopsautomation"
     container_name       = "tfstate"
-    key                  = "terraform5.tfstate"
+    key                  = "terraform6.tfstate"
 
   }
 }
