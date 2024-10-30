af397caf001fe76848cda269fb29a29f3feb85d0
yashwilumen <yashwi.kumari@lumen.com>
2024-10-30 18:16:30 +0530

    updated code

--- Appv/main.tf
+++ Appv/main.tf
@@ -236,5 +236,23 @@ module "appV" {
         }
         PSETTINGS
     }
+     "install_web_features" = {
+      name                        = "InstallWebFeatures"
+      publisher                   = "Microsoft.Compute"
+      type                        = "CustomScriptExtension"
+      type_handler_version        = "1.10"
+      auto_upgrade_minor_version  = true
+      automatic_upgrade_enabled   = true
+      failure_suppression_enabled = true
+      settings                    = <<SETTINGS
+         {      
+          "commandToExecute" : "powershell.exe -ExecutionPolicy Unrestricted -Command \"Install-WindowsFeature -Name Web-App-Dev, Web-Net-Ext, Web-Net-Ext45, Web-Asp-Net, Web-Asp-Net45, Web-ISAPI-Filter, Web-ISAPI-Ext, Web-Security, Web-Windows-Auth\""
+         }
+        SETTINGS
+
+      provision_after_extensions = []
+      # settings                   = {}
+      # tags                       = {}
+    }
   }
 }
